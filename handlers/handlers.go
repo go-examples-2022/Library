46442e037f0172a91004bd6500d85c34565cc672
Kavishankar S <kavishankar6@gmail.com>
2022-12-29 00:06:33 +0530

    12: Added HTTP server handlers for update and delete op

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -39,6 +39,9 @@ func (s *HttpHandler) BooksHandler(w http.ResponseWriter, req *http.Request) {
 	case http.MethodPost:
 		s.PostBookHandler(w, req)
 		return
+	case http.MethodDelete:
+		s.DeleteHandler(w, req)
+		return
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
@@ -92,7 +95,17 @@ func (s *HttpHandler) GetOneBookById(w http.ResponseWriter, req *http.Request) {
 }
 
 func (s *HttpHandler) PostBookHandler(w http.ResponseWriter, req *http.Request) {
+	id := strings.TrimPrefix(req.URL.Path, "/books/")
+	if id == "" {
+		// /api/v1/books
+		s.PostBookHandlerWithoutId(w, req)
+	} else {
+		// /api/v1/books/{{bookId}}
+		s.PostBookHandlerWithId(w, req)
+	}
+}
 
+func (s *HttpHandler) PostBookHandlerWithoutId(w http.ResponseWriter, req *http.Request) {
 	var b types.Book
 	err := json.NewDecoder(req.Body).Decode(&b)
 	if err != nil {
@@ -114,4 +127,64 @@ func (s *HttpHandler) PostBookHandler(w http.ResponseWriter, req *http.Request) 
 		fmt.Println("error encoding POST response", err)
 		return
 	}
+}
+
+func (s *HttpHandler) PostBookHandlerWithId(w http.ResponseWriter, req *http.Request) {
+	// Fetch Book_type from body
+	var b types.Book
+	err := json.NewDecoder(req.Body).Decode(&b)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println("error decoding POST request body", err)
+		return
+	}
+
+	// Fetch id from URL
+	id := strings.TrimPrefix(req.URL.Path, "/books/")
+
+	// Ensure book.Id and id from URL are same. Following op shouldn't be allowed
+	// endpoint => /api/v1/books/2
+	// body => {
+	//		"Name": "Book1",
+	// 		"Id": "3"
+	// }
+	if id != b.Id {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Printf("id in url (%s) doesn't match with id in body (%s) for update request\n", id, b.Id)
+		return
+	}
+
+	// Update book in Db
+	err = s.db.Update(b)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println("error persisting book", err)
+		return
+	}
+
+	// Return the updated book in response body
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(b)
+	if err != nil {
+		w.WriteHeader(http.StatusCreated)
+		fmt.Println("error encoding POST response", err)
+		return
+	}
+}
+
+func (s *HttpHandler) DeleteHandler(w http.ResponseWriter, req *http.Request) {
+	id := strings.TrimPrefix(req.URL.Path, "/books/")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println("book id not specified in request")
+		return
+	}
+	err := s.db.Delete(id)
+	if err != nil {
+		//TODO: distinguish error types and set appropiate header
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println("error deleting db entry", err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
